c1c3135d6b9d1c6e76fdda45542c5e144fd909bb
Cédric Bühler <cedric.buehler@raiffeisen.ch>
2026-01-16 23:24:57 +0100

    add application test configuration and update dependencies

--- src/test/kotlin/ch/buedev/playground/spring_boot_kafka_streams_playground/SpringBootKafkaStreamsPlaygroundApplicationTests.kt
+++ src/test/kotlin/ch/buedev/playground/spring_boot_kafka_streams_playground/SpringBootKafkaStreamsPlaygroundApplicationTests.kt
@@ -2,8 +2,18 @@ package ch.buedev.playground.spring_boot_kafka_streams_playground
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.test.context.ActiveProfiles
 
+@EmbeddedKafka(
+    brokerProperties = [
+        "auto.create.topics.enable=false",
+        "transaction.state.log.replication.factor=1",
+        "transaction.state.log.min.isr=1",
+    ],
+)
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBootKafkaStreamsPlaygroundApplicationTests {
     @Test
     fun contextLoads() {
